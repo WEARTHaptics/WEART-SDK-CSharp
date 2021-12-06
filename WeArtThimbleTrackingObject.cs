5a5f18712b790c81897b520a6d98b4e386d5628d
FrancescoAbbo <abbo.francesco.dev@gmail.com>
2021-12-06 20:29:06 +0100

    Added Tracking Thimble

---
+++ WeArtThimbleTrackingObject.cs
@@ -0,0 +1,67 @@
+
+using WeArt.Core;
+using WeArt.Messages;
+
+namespace WeArt.Components
+{
+    /// <summary>
+    /// This component receives and exposes tracking data from the hardware
+    /// </summary>
+    public class WeArtThimbleTrackingObject
+    {
+        internal HandSide _handSide = HandSide.Left;
+        internal ActuationPoint _actuationPoint = ActuationPoint.Thumb;
+
+        private WeArtClient _client;
+
+        /// <summary>
+        /// The hand side of the thimble
+        /// </summary>
+        public HandSide HandSide
+        {
+            get => _handSide;
+            set => _handSide = value;
+        }
+
+        /// <summary>
+        /// The actuation point of the thimble
+        /// </summary>
+        public ActuationPoint ActuationPoint
+        {
+            get => _actuationPoint;
+            set => _actuationPoint = value;
+        }
+
+        /// <summary>
+        /// The closure measure received from the hardware
+        /// </summary>
+        public Closure Closure
+        {
+            get;
+            private set;
+        }
+
+        public WeArtThimbleTrackingObject(WeArtClient client)
+        {
+            _client = client;
+            _client.OnConnectionStatusChanged -= OnConnectionChanged;
+            _client.OnConnectionStatusChanged += OnConnectionChanged;
+            _client.OnMessage -= OnMessageReceived;
+            _client.OnMessage += OnMessageReceived;
+        }
+
+        internal void OnConnectionChanged(bool connected)
+        {
+            Closure = new Closure() { Value = 0f };
+        }
+
+        private void OnMessageReceived(WeArtClient.MessageType type, IWeArtMessage message)
+        {
+            if (type == WeArtClient.MessageType.MessageReceived)
+            {
+                if (message is TrackingMessage trackingMessage)
+                    Closure = trackingMessage.GetClosure(HandSide, ActuationPoint);
+            }
+        }
+    }
+}
